034fd888a4fefdd25b812adb3a6bfb0b5548b439
Enrique Sotomayor <sotomaque@gmail.com>
2023-01-26 00:56:44 -1000

    why not

--- packages/web-tests/tests/index.spec.ts
+++ packages/web-tests/tests/index.spec.ts
@@ -2,9 +2,9 @@ import { test, expect } from '@playwright/test';
 
 test('homepage has a button', async ({ page }) => {
   await page.goto('http://localhost:3000');
-  const button = page.getByRole('button', {
+  const myGreatButton = page.getByRole('button', {
     name: /Boop/i,
   });
 
-  await expect(button).toBeVisible();
+  await expect(myGreatButton).toBeVisible();
 });
