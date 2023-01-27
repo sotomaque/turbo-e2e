2dcdb7a3d6bfc9209b5f2a5492a616dc41d4e40d
Enrique Sotomayor <sotomaque@gmail.com>
2023-01-26 21:50:00 -1000

    add storybook (#5)

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,3 +1,11 @@
-export const Button = () => {
-  return <button>Boop</button>;
-};
+import * as React from 'react';
+
+export interface ButtonProps {
+  children: React.ReactNode;
+}
+
+export function Button(props: ButtonProps) {
+  return <button>{props.children}</button>;
+}
+
+Button.displayName = 'Button';
