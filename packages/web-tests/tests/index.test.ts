ed0440f49365898580986f611cb3118dc99d6874
Enrique Sotomayor <sotomaque@gmail.com>
2023-01-26 19:24:12 -1000

    Second (#2)
    * testing speed
    
    * testing speed
    
    * testing speed

--- packages/web-tests/tests/index.test.ts
+++ packages/web-tests/tests/index.test.ts
@@ -2,9 +2,9 @@ import { test, expect } from '@playwright/test';
 
 test('homepage has a button', async ({ page }) => {
   await page.goto('http://localhost:3000');
-  const myGreatButton = page.getByRole('button', {
+  const button = page.getByRole('button', {
     name: /Boop/i,
   });
 
-  await expect(myGreatButton).toBeVisible();
+  await expect(button).toBeVisible();
 });
