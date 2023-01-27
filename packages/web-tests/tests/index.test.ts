a2a039be566dae34ca582b9e52ad42eb18424496
Enrique Sotomayor <sotomaque@gmail.com>
2023-01-26 22:46:54 -1000

    testing yml

--- packages/web-tests/tests/index.test.ts
+++ packages/web-tests/tests/index.test.ts
@@ -2,9 +2,9 @@ import { test, expect } from '@playwright/test';
 
 test('homepage has a button', async ({ page }) => {
   await page.goto('http://localhost:3000');
-  const myButton = page.getByRole('button', {
+  const button = page.getByRole('button', {
     name: /Boop/i,
   });
 
-  await expect(myButton).toBeVisible();
+  await expect(button).toBeVisible();
 });
