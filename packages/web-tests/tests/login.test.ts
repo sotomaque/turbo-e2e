5ae238f8fcbf13fa5a67b01060cb91cc905d1008
Enrique Sotomayor <sotomaque@gmail.com>
2023-01-28 11:46:33 -1000

    more testing

--- packages/web-tests/tests/login.test.ts
+++ packages/web-tests/tests/login.test.ts
@@ -17,18 +17,13 @@ test('Renders Correctly', async ({ page }) => {
   await expect(background).toBeVisible();
 });
 
-test('Redirects to /documents on valid login', async () => {
-  const browser = await chromium.launch({ headless: true });
-  const context = await browser.newContext({});
-  await context.route('**/*', (route) => route.continue());
-  const _page = await context.newPage();
-
+test('Redirects to /documents on valid login', async ({ page }) => {
   // SETUP /jwt/ requests are mocked to return a valid token
   const json = {
     refresh: 'mockRefreshToken',
     access: 'mockAccessToken',
   };
-  await _page.route(`**/api/v1/jwt/`, (route) =>
+  await page.route(`**/api/v1/jwt/`, (route) =>
     route.fulfill({
       status: 200,
       contentType: 'application/json',
@@ -37,51 +32,43 @@ test('Redirects to /documents on valid login', async () => {
   );
 
   // GIVEN the user is on the login _page
-  await _page.goto('http://localhost:3000/login');
+  await page.goto('http://localhost:3000/login');
 
   // WHEN the user fills the form and submits
-  await _page.locator('[data-test-id="email-input"]').click();
-  await _page
+  await page.locator('[data-test-id="email-input"]').click();
+  await page
     .locator('[data-test-id="email-input"]')
     .fill('test@earnbetter.com');
-  await _page.locator('[data-test-id="email-input"]').press('Tab');
-  await _page.locator('[data-test-id="password-input"]').fill('test');
-  await _page.locator('[data-test-id="submit-button"]').click();
+  await page.locator('[data-test-id="email-input"]').press('Tab');
+  await page.locator('[data-test-id="password-input"]').fill('test');
+  await page.locator('[data-test-id="submit-button"]').click();
 
   // THEN the user is redirected to /documents
-  await expect(_page).toHaveURL('http://localhost:3000/documents');
+  await expect(page).toHaveURL('http://localhost:3000/documents');
 });
 
-test('Shows error message on invalid login attemp', async () => {
-  test.slow(); // Easy way to triple the default timeout
-
-  // TODO: do i actually need this setup?
-  const browser = await chromium.launch({ headless: true });
-  const context = await browser.newContext({});
-  await context.route('**/*', (route) => route.continue());
-  const _page = await context.newPage();
-
+test('Shows error message on invalid login attemp', async ({ page }) => {
   // GIVEN the user is on the login _page
-  await _page.goto('http://localhost:3000/login');
+  await page.goto('http://localhost:3000/login');
 
   // WHEN the user fills the form and submits
-  await _page.locator('[data-test-id="email-input"]').click();
-  await _page
+  await page.locator('[data-test-id="email-input"]').click();
+  await page
     .locator('[data-test-id="email-input"]')
     .fill('test@earnbetter.com');
-  await _page.locator('[data-test-id="email-input"]').press('Tab');
-  await _page.locator('[data-test-id="password-input"]').fill('test');
-  const responsePromise = _page.waitForResponse(
+  await page.locator('[data-test-id="email-input"]').press('Tab');
+  await page.locator('[data-test-id="password-input"]').fill('test');
+  const responsePromise = page.waitForResponse(
     (resp) => resp.url().includes('/jwt/') && resp.status() === 401
   );
 
-  await _page.locator('[data-test-id="submit-button"]').click();
+  await page.locator('[data-test-id="submit-button"]').click();
 
   // THEN we should see a 401
   // with the error message "Whoops! Those credentials don`t look right."
   await responsePromise;
-  await expect(_page).toHaveURL('http://localhost:3000/login');
-  const errorMessage = _page.getByTestId('error-message');
+  await expect(page).toHaveURL('http://localhost:3000/login');
+  const errorMessage = page.getByTestId('error-message');
   await expect(errorMessage).toBeVisible();
   await expect(errorMessage).toHaveText(
     'Whoops! Those credentials don`t look right.'
