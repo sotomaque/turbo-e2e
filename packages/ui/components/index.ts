d31e2178383043e8be1e616ceb11e0b8097d7cae
Enrique Sotomayor <sotomaque@gmail.com>
2023-02-01 20:10:34 -1000

    document preview

--- packages/ui/components/index.ts
+++ packages/ui/components/index.ts
@@ -2,6 +2,8 @@ export * from './AccountDropdown';
 export * from './AppLayout';
 export * from './Button';
 export * from './DesktopMenu';
+export * from './Dialog';
+export * from './DocumentThumbnail';
 export * from './DropdownMenu';
 export * from './DropdownMenuItem';
 export * from './Header';
